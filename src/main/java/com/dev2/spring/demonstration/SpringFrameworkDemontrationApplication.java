f824646df1f695e5297a205ebcedbc8f6f2cf913
Luis Henrique Hendges <luishendges@outlook.com>
2020-06-11 22:50:01 -0300

    Initial Commit

---
+++ src/main/java/com/dev2/spring/demonstration/SpringFrameworkDemontrationApplication.java
@@ -0,0 +1,13 @@
+package com.dev2.spring.demonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkDemontrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkDemontrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev2/spring/demonstration/SpringFrameworkDemontrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev2.spring.demonstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkDemontrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
